11f78ee3584c14b3085f4b8b6db17138dd01ae13
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-09-27 17:09:36 +0200

    fixed style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -11,8 +11,8 @@
             <div class="row mx-0 my-4 justify-content-around">
                 @foreach ($comics as $index => $comic)
                     <div class="col-3 px-0 my-4 px-4">
-                        <div class="card cardBox border-2">
-                            <img src="{{ $comic['thumb'] }}" class="card-img object-fit-cover"></img>
+                        <div class="card cardBox border-2 h-100">
+                            <img src="{{ $comic['thumb'] }}" class="card-img" loading="lazy"></img>
 
                             <div class="card-img-overlay cardInfoBox text-white fw-bold text-center p-2 bg-black lh-lg">
 
@@ -52,3 +52,27 @@
         </div>
     </div>
 @endSection
+
+@section('css')
+    <style>
+        .cardBox:hover {
+            transform: scale(1.1);
+            transition: 400ms;
+
+        }
+
+        .cardInfoBox {
+            opacity: 0;
+        }
+
+        .cardInfoBox:hover {
+            opacity: 1;
+        }
+
+        .card img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    </style>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="/style.css">
     <title>@yield('title', 'Hello Laravel World')</title>
     @vite('resources/js/app.js')
 </head>
@@ -14,6 +20,8 @@
     @include('partials.header')
 
     @yield('content')
+
+    @yield('css')
 
     @include('partials.footer')
 
